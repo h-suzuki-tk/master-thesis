093af2f89094bf756c52105fd815a74b2e777b4f
suzuki.hina <hina.suzuki.298@gmail.com>
2021-03-21 23:08:32 +0900

    実行時間計測スクリプトexetime.shの実装

---
+++ expt/exetime.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+LOG_DIR=log;
+NEED_ARG_NUM=3;
+
+if [ $# -ne ${NEED_ARG_NUM} ] 
+then
+
+	echo "Arguments:"
+	echo "* Command"        # $1
+	echo "* Log file name"  # $2
+	echo "* Execution time" # $3
+
+else
+
+	CMD=$1
+	LOG_FILE=$2
+	EXE_NUM=$3
+
+	mkdir -p ${LOG_DIR}
+	: > ${LOG_DIR}/${LOG_FILE}
+
+	echo "Arguments:"
+	echo "* Command: ${CMD}"
+	echo "* Execution time: ${EXE_NUM}"
+
+	echo "Starting execitions ..."
+	time ( i=1; while [ ${i} -le ${EXE_NUM} ]; 
+		do 
+		${CMD} &>> ${LOG_DIR}/${LOG_FILE}
+		echo -e -n "\n\n\n" &>> ${LOG_DIR}/${LOG_FILE}; 
+		i=$(expr ${i} + 1); done
+	);
+	echo -e "\n... ended."
+	
+	echo "The log was written in ${LOG_DIR}/${LOG_FILE}"
+	echo -e -n "\n"
+
+fi
