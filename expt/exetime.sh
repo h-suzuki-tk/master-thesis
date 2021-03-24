98bb48dd26b83b24904220bb5b987261eb5a32b9
suzuki.hina <hina.suzuki.298@gmail.com>
2021-03-24 13:52:36 +0900

    実験用データセットの追加／exetime.shの更新

--- expt/exetime.sh
+++ expt/exetime.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-LOG_DIR=log/search;
 NEED_ARG_NUM=3;
 
 if [ $# -ne ${NEED_ARG_NUM} ] 
@@ -7,7 +6,7 @@ then
 
 	echo "Arguments:"
 	echo "* Command"        # $1
-	echo "* Log file name"  # $2
+	echo "* Log file path"  # $2
 	echo "* Execution time" # $3
 
 else
@@ -16,23 +15,20 @@ else
 	LOG_FILE=$2
 	EXE_NUM=$3
 
-	mkdir -p ${LOG_DIR}
-	: > ${LOG_DIR}/${LOG_FILE}
+	: > ${LOG_FILE}
 
 	echo "Arguments:"
 	echo "* Command: ${CMD}"
+	echo "* Log file path: ${LOG_FILE}"
 	echo "* Execution time: ${EXE_NUM}"
 
-	echo "Starting execitions ..."
 	time ( i=1; while [ ${i} -le ${EXE_NUM} ]; 
 		do 
-		${CMD} &>> ${LOG_DIR}/${LOG_FILE}
-		echo -e -n "\n\n\n" &>> ${LOG_DIR}/${LOG_FILE}; 
+		${CMD} &>> ${LOG_FILE}
+		echo -e -n "\n\n\n" &>> ${LOG_FILE}; 
 		i=$(expr ${i} + 1); done
 	);
-	echo -e "\n... ended."
-	
-	echo "The log was written in ${LOG_DIR}/${LOG_FILE}"
+
 	echo -e -n "\n"
 
 fi
